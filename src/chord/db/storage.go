2b474cd063b391dd430b6ca45b27839b3bc84644
ZhuojunChen <zhuoj.ch@gmail.com>
2020-05-22 14:53:36 -0700

    Implement Get / Set / Keys

--- src/chord/db/storage.go
+++ src/chord/db/storage.go
@@ -1,5 +1,10 @@
 package db
 
+import (
+	"strings"
+	"sync"
+)
+
 type KV struct {
 	k string
 	v string
@@ -8,6 +13,12 @@ type KV struct {
 type Pattern struct {
 	prefix string
 	suffix string
+}
+
+func (p *Pattern) Match(k string) bool {
+	ret := strings.HasPrefix(k, p.prefix)
+	ret = ret && strings.HasSuffix(k, p.suffix)
+	return ret
 }
 
 type List struct {
@@ -21,4 +32,52 @@ type Storage interface {
 	Set(kv KV, ok *bool) error
 	// Keys returns the keys matched the given pattern
 	Keys(p Pattern, list *List) error
+}
+
+// In-memory storage implementation. All calls always returns nil.
+type Store struct {
+	strs  map[string]string		// strs[key] = value
+
+	strLock   sync.Mutex
+}
+
+func (self *Store) Get(key string, value *string) error {
+	self.strLock.Lock()
+	defer self.strLock.Unlock()
+
+	*value = self.strs[key]
+
+	return nil
+}
+
+func (self *Store) Set(kv *KV, ok *bool) error {
+	self.strLock.Lock()
+	defer self.strLock.Unlock()
+
+	if kv.v != "" {
+		self.strs[kv.k] = kv.v
+	} else {
+		delete(self.strs, kv.k)
+	}
+
+	*ok = true
+
+	return nil
+}
+
+func (self *Store) Keys(p *Pattern, r *List) error {
+	self.strLock.Lock()
+	defer self.strLock.Unlock()
+
+	ret := make([]string, 0, len(self.strs))
+
+	for k := range self.strs {
+		if p.Match(k) {
+			ret = append(ret, k)
+		}
+	}
+
+	r.L = ret
+
+	return nil
 }
